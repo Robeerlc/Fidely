0bca473be4ca00cbe83d288b6ea97ec323579da1
Roberto Lucas <lucascastilloroberto@gmail.com>
2026-06-25 11:54:23 +0200

    Subida proyecto inicial

---
+++ src/test/java/com/fidely/FidelyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fidely;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FidelyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
