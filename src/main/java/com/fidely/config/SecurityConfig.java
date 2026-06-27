0b94d4f8da2f6d520d505e029f44e17308fe8e7b
Roberto Lucas <lucascastilloroberto@gmail.com>
2026-06-27 22:44:41 +0200

    made swagger public

--- src/main/java/com/fidely/config/SecurityConfig.java
+++ src/main/java/com/fidely/config/SecurityConfig.java
@@ -37,6 +37,9 @@ public class SecurityConfig {
                         .requestMatchers("/api/v1/business/login").permitAll()
                         .requestMatchers("/api/v1/wallet/*/download").permitAll()
                         .requestMatchers("/api/v1/wallet/onboarding").permitAll()
+                        .requestMatchers("/swagger-ui.html").permitAll()
+                        .requestMatchers("/swagger-ui/index.html").permitAll()
+
 
                         // Rutas Privadas
                         // Gestión de empleados (Solo Dueño)
